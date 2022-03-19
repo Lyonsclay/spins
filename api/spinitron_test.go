d7514e3284578c9ddd82c3205286e3b83145f445
Lyonsclay <lyonsclay@yahoo.com>
2022-03-19 14:25:03 -0400

    Rename

--- api/spinitron_test.go
+++ api/spinitron_test.go
@@ -19,6 +19,17 @@ func TestSpinitronParse(t *testing.T) {
 		t.Errorf("Expected: %v \n Received: %v \n", expected, output)
 	}
 }
+var resources = []string{
+	"https://spinitron.com/WXOX/",
+	"https://spinitron.com/WXOX/dj/103719/Ben-Zoeller",
+	"https://spinitron.com/WXOX/show/222061/World-Workout-w-The-Dream-Team",
+	"https://spinitron.com/WXOX/pl/15396116/Hello-Goodbye",
+	"https://spinitron.com/WXOX/calendar",
+	"https://spinitron.com/WXOX/calendar-feed?timeslot=15&start=2022-02-20T00:00:00&end=2022-02-27T06:59:59&_=1645467101614",
+	"https://spinitron.com/WXOX/calendar-feed?start=2022-02-20T00:00:00&end=2022-02-27T06:59:59",
+	"http://widgets.spinitron.com/widget/now-playing-v2?callback=_spinitron04669496113876874164549461312&station=wxox&num=1&sharing=0&player=0&cover=0",
+}
+
 
 func TestCalendar(t *testing.T) {
 	cal := resources[6]
