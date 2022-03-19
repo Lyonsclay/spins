eacde14499f8d8842da58c0f164369bebff6021a
Lyonsclay <lyonsclay@yahoo.com>
2022-03-19 14:27:45 -0400

    Add interpolation

--- api/shownuff.go
+++ api/shownuff.go
@@ -11,5 +11,5 @@ import (
 
 func Handler(w http.ResponseWriter, r *http.Request) {
     currentTime := time.Now().Format(time.RFC850)
-    fmt.Fprintf(w, currentTime)
+    fmt.Fprintf(w,"%v", currentTime)
 }
