258a840b9b5c90b361aeefc33406294d46916757
Lyonsclay <lyonsclay@yahoo.com>
2022-03-21 19:01:10 -0400

    Strip down

--- api/show.go
+++ api/show.go
@@ -4,10 +4,10 @@ import (
 	"fmt"
 	// "github.com/anaskhan96/soup"
 	"net/http"
-	"net/url"
+	// "net/url"
 )
 
-func GetShow(path string) string {
+// func GetShow(path string) string {
 	// res, err := soup.Get(path)
 	// if err != nil {
 	// 	panic(err)
@@ -15,16 +15,16 @@ func GetShow(path string) string {
 	// doc := soup.HTMLParse(res)
 	// body := doc.Find("body")
 	// return body.Text()
-	return path
-}
+	// return path
+// }
 
-func Harphandler(w http.ResponseWriter, r *http.Request) {
-	u := r.URL.RawQuery
-	p, err := url.ParseQuery(u)
-	if err != nil {
-		panic(err)
-	}
-	path := p["path"]
+func ShowHandler(w http.ResponseWriter, r *http.Request) {
+	// u := r.URL.RawQuery
+	// p, err := url.ParseQuery(u)
+	// if err != nil {
+	// 	panic(err)
+	// }
+	// path := p["path"]
 
-	fmt.Fprintf(w, "{\"path\": \"%v\"}", path)
+	fmt.Fprintf(w, "{\"path\": \"%v\"}", "path")
 }
